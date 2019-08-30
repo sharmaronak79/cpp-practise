6fd72b6bbbde57b4b3f2027a5017f4ee6e6b2576
ronak <sharmaronak79@yahoo.com>
2019-08-30 02:33:20 -0400

    arrayprac.cpp

---
+++ arrayprac.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={14,20,35,9,17};
+
+for(int i=0;i<5;i++)
+{
+    cout<<"value of arr["<<i<<"]"<<"="<<arr[i]<<endl;
+}
+
+for(int i=0;i<5;i++)
+{
+    cout<<"address of arr["<<i<<"]"<<"="<<&arr[i]<<endl;
+}
+
+    return 0;
+}
