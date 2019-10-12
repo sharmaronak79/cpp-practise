2c8a84986ebce7438a493b41244cb6b21e4b5f1d
ronak <sharmaronak79@yahoo.com>
2019-10-11 21:33:31 -0400

    pass_manual.cpp

---
+++ pass_manual.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string pass = "password";
+    string input;
+    string key;
+    cout << "What is your password: " << endl;
+    cin >> input;
+    cout<<"unlock key? : " << endl;
+    cin>>key;
+    if (input==key){
+        cout << "Correct" << endl;
+    }else{
+        cout << "Wrong" << endl;
+    }
+    return 0;
+}
