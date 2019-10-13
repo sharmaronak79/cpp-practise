859f8dd31df014c5012d9fb82c4121c591382a21
ronak <sharmaronak79@yahoo.com>
2019-10-12 22:29:57 -0400

    project_pass1.cpp

---
+++ project_pass1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+
+int main()
+{int i;
+    cout<< "welcome to access world"<<endl;
+    cout<< " 1- for new regestration \n 2-for log in \n 3- for exit" << endl;
+
+    cout << "enter yot r choice : ";
+cin>>i;
+
+if (i = 1)
+
+    cout << "wel come for regestration"<< endl;
+
+else if (i = 2)
+    cout <<"welcome to log in page"<<endl;
+
+else if (i = 3)
+
+    cout << "see you next time"<< endl;
+
+else
+
+cout <<"exit"<<endl;
+
+return 0;
+}
