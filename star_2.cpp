f856f2d6e2bb759df2aec92bd32bbe3c81563c9b
ronak <sharmaronak79@yahoo.com>
2019-10-21 22:18:29 -0400

    star_2.cpp

---
+++ star_2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int rows, i, j, space;
+
+   cout << "Enter number of rows: ";
+   cin >> rows;
+
+   for(i = rows; i >= 1; i--)
+   {
+      //for loop to put space
+      for(space = i; space < rows; space++)
+         cout << " ";
+      //for loop for displaying star
+      for(j = 1; j <= (2 * i - 1); j++)
+         cout << "* ";
+
+      cout << "\n";
+   }
+
+   return 0;
+}
