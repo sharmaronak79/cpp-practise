d3efd952a9ccc6310f3c17a2320c1079bd525e20
ronak <sharmaronak79@yahoo.com>
2020-05-04 21:06:19 -0400

    strongnumber.c

---
+++ strongnumber.c
@@ -0,0 +1,34 @@
+/* if a number is equal to its sum of individual digit's factorial like 145=1!+4!+5! then it is storng number*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n,fact,sum=0,r,i,x;
+    printf("Enter number to check storng or not : ");
+    scanf("%d",&n);
+    x=n;
+    
+    while(n>0)
+    {
+        fact=1;
+        r=n%10;
+        for(i=1;i<=r;i++)
+        {
+            fact=fact*i;
+        }
+        sum=sum+fact;
+        n=n/10;
+    }
+     if(x==sum)
+    {
+        printf("\n %d is a strong number",x);
+    
+    
+    }
+    else
+    {
+        printf("\n %d is not a strong number",x);
+    }
+    return 0;
+}
