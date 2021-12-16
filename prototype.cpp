f42822b8f5f2693b996f30e742e660c4a515429d
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2021-12-16 06:47:59 -0500

    Update prototype.cpp

--- prototype.cpp
+++ prototype.cpp
@@ -5,3 +5,17 @@ the code of function declaration should be before the function call.
  However, if we want to define a function after the function call,
 we need to use the function prototype. 
 */
+
+// function prototype
+void add(int, int);
+
+int main() {
+    // calling the function before declaration.
+    add(5, 3);
+    return 0;
+}
+
+// function definition
+void add(int a, int b) {
+    cout << (a + b);
+}
