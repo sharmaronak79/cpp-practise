4316fe3747ce1a1c4e8f777dad78eea8f475425f
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2021-12-16 06:46:59 -0500

    Create prototype.cpp

---
+++ prototype.cpp
@@ -0,0 +1,7 @@
+//Function Prototype
+/*
+In C++, 
+the code of function declaration should be before the function call.
+ However, if we want to define a function after the function call,
+we need to use the function prototype. 
+*/
