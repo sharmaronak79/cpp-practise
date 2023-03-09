b1f9bb3f4882441fd2cbf8718a0a3ee8a56c2257
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2023-03-08 23:39:50 -0500

    Add files via upload

---
+++ section4/First_Program/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a;
+    cout << "Enter your Favourite Number betwenn 1 and 100 : ";
+    cin >> a;
+    cout << "Amazing!! That's my favorite number too!"<<endl;
+    cout << "No really!!," << a <<" is my favourite number too"<<endl;
+    return 0;
+}
---
+++ section4/project2/main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello Ronak"<<endl;
+    cout<<"Hello Project2"<<endl;
+    
+    
+    return 0;
+}
