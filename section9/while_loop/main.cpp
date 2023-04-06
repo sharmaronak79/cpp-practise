0b465741d91f160de9ec28ab05039a505c31a644
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2023-04-05 22:17:04 -0400

    Add files via upload

---
+++ section9/while_loop/main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello Ronak"<<endl;
+    
+//    int i {0};
+//    while(i<=5){
+//        cout<<i<<endl;
+//        i++;
+//    }
+    int i{10};
+    while(i>0){
+        cout<<i<<endl;
+        i--;
+    }
+
+    cout<<"Thats it!!!"<<endl;
+    return 0;
+}
