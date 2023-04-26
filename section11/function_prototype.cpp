047cd9c94b4f5b5d5558fc9610487a64f6ebe86a
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2023-04-25 23:14:11 -0400

    Create function_prototype.cpp

---
+++ section11/function_prototype.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+//----WRITE YOUR FUNCTION PROTOTYPES BELOW THIS LINE----
+double fahrenheit_to_celsius(double temperature);
+double fahrenheit_to_kelvin(double temperature);
+
+
+//----WRITE YOUR FUNCTION PROTOTYPES ABOVE THIS LINE----
+//----DO NOT MODIFY THE CODE BELOW THIS LINE----
+double fahrenheit_to_celsius(double temperature){
+    temperature = (5.0/9.0)*(temperature - 32);
+    return round(temperature);
+}
+
+double fahrenheit_to_kelvin(double temperature){
+    temperature = (5.0/9.0)*(temperature - 32) + 273;
+    return round(temperature);
+}
+
+void temperature_conversion(double fahrenheit_temperature) {
+    
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR FUNCTION CALLS BELOW THIS LINE----
+    double celsius_temperature = fahrenheit_to_celsius( fahrenheit_temperature);
+    double kelvin_temperature = fahrenheit_to_kelvin( fahrenheit_temperature);
+    
+    //----WRITE YOUR FUNCTION CALLS ABOVE THIS LINE----
+    //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+    
+    cout << "The fahrenheit temperature " << fahrenheit_temperature << " degrees is equivalent to " << celsius_temperature << " degrees celsius and " << kelvin_temperature << " degrees kelvin.";
+}
+
+//----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+//----WRITE YOUR FUNCTION DEFINITIONS BELOW THIS LINE----
+
+
