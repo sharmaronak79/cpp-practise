13c4c53bbf89eaa3f196a77833e24a40fb2d16fe
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2023-06-07 00:38:54 -0400

    Create pointer.cpp

---
+++ section12/pointer.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+cout<<"Hello Ronak"<<endl;
+cout<<"Hello Ronak"<<endl;
+    int *p1 {nullptr};
+    double *p2 {nullptr};
+    unsigned long long *p3{nullptr};
+    vector<string> *p4{nullptr};
+    string *p5{nullptr};
+    
+    cout<< sizeof(p1)<<endl;
+    cout<< sizeof(p2)<<endl;
+    cout<< sizeof(p3)<<endl;
+    cout<< sizeof(p4)<<endl;
+    cout<< sizeof(p5)<<endl;
+        
+
+    
+    return 0;
+} 
