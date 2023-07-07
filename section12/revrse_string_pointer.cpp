413645e8dc9e9571c95a4c2b694f246762235a52
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2023-07-06 22:58:04 -0400

    Update revrse_string_pointer.cpp

--- section12/revrse_string_pointer.cpp
+++ section12/revrse_string_pointer.cpp
@@ -1,10 +1,9 @@
-// You do NOT have to write a main() function. The main() function is written by me
-// behind the scenes to call your function and test your code.
+
 #include <string>
 
 std::string reverse_string(const std::string &str) {
     std::string reversed;
-    // Write your code below this line 
+  
  const char *start= str.c_str();
  const char *end = str.c_str()+str.size()-1;
  
@@ -12,6 +11,6 @@ std::string reverse_string(const std::string &str) {
      reversed.push_back(*end);
      end--;
  }
-    // Write your code abocve this line
+    
     return reversed;
 }
