8b9cc944b21ff0414a49b36c0e76b117df8428d3
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2023-07-30 14:27:46 -0400

    Create class_object.cpp

---
+++ section13/class_object.cpp
@@ -0,0 +1,44 @@
+//Section13
+//Declare and Class Object
+
+#include <iostream>
+#include <string>
+#include<vector>
+
+using namespace std;
+
+class Player{
+//Attributes
+string name;
+int health;
+int xp;
+
+//Methods
+void talks(string);
+bool is_dead();
+};
+
+class Account{
+    //Attributes
+    
+
+    //Method
+};
+
+int main(){
+    cout<<"Hello Ronak"<<endl;
+
+    Player frank;
+    Player hero;
+
+    Player players[]{frank,hero};
+
+    vector <Player> player_vec{frank};
+    player_vec.push_back(hero);
+
+    Player *enemy{nullptr};
+    enemy = new Player;
+
+
+    return 0;
+}
